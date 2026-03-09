5009d87f105592b3a19916526cfd8608564022e6
shavkatdevv <shavkatomonov4424@gmail.com>
2026-03-09 14:32:57 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="shadow">
+        <div className="container-fluid py-4">
+          <img src="https://olcha.uz/_nuxt/plus.lRzD4Jf7.png" alt="" />
+        </div>
+      </header>
+
+      <section id="products" className="py-3"> 
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+              <img className="d-block productImg" src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/BIg37oXN6kB7cLEFbodLmQLwVoT80TSKXxbqR2L39mBLMsMJKwaxRxHYZkEx.jpg" alt="" />
+              <a href="#" className="groupName d-block mb-2 mt-3">Smartfon</a>
+              <p className="price mb-0">1 550 000</p>
+              <p className="priceMonth">182 000 so'm x 12 oy</p>
+              <p className="title">Samsung Galaxy</p>
+              <a href="#" className="btn">Savatcha</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+              <img className="d-block productImg" src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/BIg37oXN6kB7cLEFbodLmQLwVoT80TSKXxbqR2L39mBLMsMJKwaxRxHYZkEx.jpg" alt="" />
+              <a href="#" className="groupName d-block mb-2 mt-3">Smartfon</a>
+              <p className="price mb-0">1 550 000</p>
+              <p className="priceMonth">182 000 so'm x 12 oy</p>
+              <p className="title">Samsung Galaxy</p>
+              <a href="#" className="btn">Savatcha</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+              <img className="d-block productImg" src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/BIg37oXN6kB7cLEFbodLmQLwVoT80TSKXxbqR2L39mBLMsMJKwaxRxHYZkEx.jpg" alt="" />
+              <a href="#" className="groupName d-block mb-2 mt-3">Smartfon</a>
+              <p className="price mb-0">1 550 000</p>
+              <p className="priceMonth">182 000 so'm x 12 oy</p>
+              <p className="title">Samsung Galaxy</p>
+              <a href="#" className="btn">Savatcha</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+              <img className="d-block productImg" src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/BIg37oXN6kB7cLEFbodLmQLwVoT80TSKXxbqR2L39mBLMsMJKwaxRxHYZkEx.jpg" alt="" />
+              <a href="#" className="groupName d-block mb-2 mt-3">Smartfon</a>
+              <p className="price mb-0">1 550 000</p>
+              <p className="priceMonth">182 000 so'm x 12 oy</p>
+              <p className="title">Samsung Galaxy</p>
+              <a href="#" className="btn">Savatcha</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+              <img className="d-block productImg" src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/BIg37oXN6kB7cLEFbodLmQLwVoT80TSKXxbqR2L39mBLMsMJKwaxRxHYZkEx.jpg" alt="" />
+              <a href="#" className="groupName d-block mb-2 mt-3">Smartfon</a>
+              <p className="price mb-0">1 550 000</p>
+              <p className="priceMonth">182 000 so'm x 12 oy</p>
+              <p className="title">Samsung Galaxy</p>
+              <a href="#" className="btn">Savatcha</a>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
